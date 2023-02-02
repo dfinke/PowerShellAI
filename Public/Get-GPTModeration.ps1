a69bb70d1be89b797ba52576ed6f0f10c27ca013
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2023-02-02 12:11:39 -0500

    added function for moderation test

---
+++ Public/Get-GPTModeration.ps1
@@ -0,0 +1,38 @@
+function Get-GPTModeration {
+	<#
+	.SYNOPSIS
+	Checks if prompt contains wording that violates OpenAI moderation rules
+	.DESCRIPTION
+	Checks prompt text content against latest moderation rules to determine if
+	any OpenAI moderation rules would be violated.
+	.PARAMETER InputText
+	Prompt text to evaluate
+	.PARAMETER Endpoint
+	URI for API moderations namespace. Exposed in case a different endpoint is needed later
+	.EXAMPLE
+	Get-GPTModeration -InputText "I want to kill them."
+	.NOTES
+	This function requires the 'OpenAIKey' environment variable to be defined before being invoked
+	#>
+	[CmdletBinding()]
+	param(
+		[parameter()][string][ValidateNotNullOrEmpty()]$InputText,
+		[parameter()][string]$Endpoint = "https://api.openai.com/v1/moderations"
+	)
+	try {
+		if ([string]::IsNullOrWhiteSpace($env:OpenAIKey)) { throw "OpenAIKey environment variable is not defined" }
+		if ([string]::IsNullOrWhiteSpace($Endpoint)) { throw "Endpoint was not provided" }
+		$headers = @{
+			"Authorization" = "Bearer $($env:OpenAIKey)"
+			"Content-Type" = "application/json"
+		}
+		$response = Invoke-RestMethod -Method POST -Uri $Endpoint -Headers $headers -Body '{"input": "'+$InputText+'"}'
+		if ($NamesOnly) {
+			return ($response.Content | ConvertFrom-Json).data.id
+		} else {
+			return ($response.Content | ConvertFrom-Json).data
+		}
+	} catch {
+		Write-Error $_.Exception.Message
+	}
+}
