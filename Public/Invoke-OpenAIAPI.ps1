868191789df81e8e053424e840a6d598867ea824
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-28 13:40:56 +0100

    Update help; Add error handling

--- Public/Invoke-OpenAIAPI.ps1
+++ Public/Invoke-OpenAIAPI.ps1
@@ -1,19 +1,19 @@
 function Invoke-OpenAIAPI {
     <#
     .SYNOPSIS
-    Invoke the OpenAI API
+    Invoke the OpenAI API.
 
     .DESCRIPTION
-    Invoke the OpenAI API
+    Invoke the OpenAI API.
 
     .PARAMETER Uri
-    The URI to invoke
+    The URI to invoke.
 
     .PARAMETER Method
-    The HTTP method to use. Defaults to 'Get'
+    The HTTP method to use. Defaults to 'Get'.
 
     .PARAMETER Body
-    The body to send with the request
+    The body to send with the request.
 
     .EXAMPLE
     Invoke-OpenAIAPI -Uri "https://api.openai.com/v1/images/generations" -Method Post -Body $body
@@ -27,25 +27,29 @@ function Invoke-OpenAIAPI {
         $Body
     )
 
-    if (!(Test-OpenAIKey)) {
-        throw 'Please set your OpenAI API key using Set-OpenAIKey or by configuring the $env:OpenAIKey environment variable (https://beta.openai.com/account/api-keys)'
-    }
+    try {
+        if (!(Test-OpenAIKey)) {
+            throw 'Please set your OpenAI API key using Set-OpenAIKey or by configuring the $env:OpenAIKey environment variable (https://beta.openai.com/account/api-keys)'
+        }
 
-    $params = @{
-        Uri         = $Uri
-        Method      = $Method
-        ContentType = 'application/json'
-        body        = $Body
-    }
+        $params = @{
+            Uri         = $Uri
+            Method      = $Method
+            ContentType = 'application/json'
+            body        = $Body
+        }
 
-    if (($apiKey = Get-OpenAIKey) -is [SecureString]) {
-        #On PowerShell 6 and higher use Invoke-RestMethod with Authentication parameter and secure Token
-        $params['Authentication'] = 'Bearer'
-        $params['Token'] = $apiKey
-    } else {
-        #On PowerShell 5 and lower, or when using the $env:OpenAIKey environment variable, use Invoke-RestMethod with plain text header
-        $params['Headers'] = @{Authorization = "Bearer $apiKey"}
-    }
+        if (($apiKey = Get-OpenAIKey) -is [SecureString]) {
+            #On PowerShell 6 and higher use Invoke-RestMethod with Authentication parameter and secure Token
+            $params['Authentication'] = 'Bearer'
+            $params['Token'] = $apiKey
+        } else {
+            #On PowerShell 5 and lower, or when using the $env:OpenAIKey environment variable, use Invoke-RestMethod with plain text header
+            $params['Headers'] = @{Authorization = "Bearer $apiKey"}
+        }
 
-    Invoke-RestMethod @params
+        Invoke-RestMethod @params
+    } catch {
+        Write-Error -ErrorRecord $_ -ErrorAction $ErrorActionPreference
+    }
 }
