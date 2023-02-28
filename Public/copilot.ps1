e8a65d755b54cdf2943911b0ba5781cdb3f8c992
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-28 12:58:40 +0100

    Add error handling; Add missing parameter and update help; Splat parameters; Minor code refactoring

--- Public/copilot.ps1
+++ Public/copilot.ps1
@@ -1,15 +1,18 @@
 function copilot {
     <#
         .SYNOPSIS
-        Use GPT to help you remember PowerShell commands and other command line tools
+        Use GPT to help you remember PowerShell commands and other command line tools.
 
         .DESCRIPTION
-        Makes the request to GPT, parses the response and displays it in a box and then prompts the user to run the code or not
+        Makes the request to GPT, parses the response and displays it in a box and then prompts the user to run the code or not.
+
+        .PARAMETER Raw
+        Returns the raw JSON response from the API and does not prompt for choice.
 
         .EXAMPLE
         # via https://twitter.com/ClemMesserli/status/1616312238209376260?s=20&t=KknO2iPk3yrQ7x42ZayS7g
 
-        copilot "using PowerShell regex, just code. split user from domain of email address with match:  demo.user@google.com"
+        copilot "using PowerShell regex, just code. split user from domain of email address with match: demo.user@google.com"
 
         .EXAMPLE
         copilot 'how to get ImportExcel'
@@ -22,9 +25,9 @@ function copilot {
 
         .EXAMPLE
         copilot 'for each file in the current dir list the name and length'
-        
+
         .EXAMPLE
-        copilot 'Find all enabled users that have a samaccountname similar to Mazi; List SAMAccountName and DisplayName'
+        copilot 'Find all enabled users that have a SamAccountName similar to Mazi; List SamAccountName and DisplayName'
     #>
     param(
         [Parameter(Mandatory)]
@@ -36,57 +39,58 @@ function copilot {
         # Don't show prompt for choice
         [Switch]$Raw
     )
-    
-    # $inputPrompt = $args -join ' '
-    
-    $shell = 'powershell, just code:'
-    
-    $promptComments = ', include comments'
-    if (-not $IncludeComments) {
-        $promptComments = ''
-    }
 
-    $prompt = "using {0} {1}: {2}`n" -f $shell, $promptComments, $inputPrompt
-    $prompt += '```'
+    try {
+        $shell = 'powershell, just code:'
 
-    $completion = Get-GPT3Completion -prompt $prompt -max_tokens $max_tokens -temperature $temperature -stop '```'
-    $completion = $completion -split "`n"
-    
-    if ($completion[0] -ceq 'powershell') {
-        $completion = $completion[1..($completion.Count - 1)]
-    }
+        $promptComments = ', include comments'
+        if (-not $IncludeComments) {
+            $promptComments = ''
+        }
 
-    if ($Raw) {
-        return $completion
-    }
-    else {
-        $result = @($inputPrompt)
-        $result += ''
-        $result += $completion
+        $prompt = "using {0} {1}: {2}`n{3}" -f $shell, $promptComments, $inputPrompt, '```'
 
-        $result | CreateBoxText
+        $splatParams = @{
+            prompt      = $prompt
+            max_tokens  = $max_tokens
+            temperature = $temperature
+            stop        = '```'
+            ErrorAction = 'Stop'
+        }
+        $completion = (Get-GPT3Completion @splatParams).Split("`n")
 
-        $userInput = CustomReadHost
+        if ($completion[0] -ceq 'powershell') {
+            $completion = $completion[1..($completion.Count - 1)]
+        }
 
-        if ($userInput -eq 0) {
-            $runnable = for ($idx = 1; $idx -lt $result.Count; $idx++) {
-                $line = $result[$idx]
-                if ([string]::IsNullOrEmpty($line)) {
-                    continue
+        if ($Raw) {
+            return $completion
+        } else {
+            ($result = @($inputPrompt) + $completion) | CreateBoxText
+            $userInput = CustomReadHost
+
+            if ($userInput -eq 0) {
+                $runnable = for ($idx = 1; $idx -lt $result.Count; $idx++) {
+                    $line = $result[$idx]
+                    if ([string]::IsNullOrEmpty($line)) {
+                        continue
+                    }
+
+                    $line = $line.Trim()
+                    if ($line.StartsWith('#')) {
+                        continue
+                    }
+
+                    $line
                 }
 
-                $line = $line.Trim()
-                if ($line.StartsWith('#')) {
-                    continue
-                }
-
-                $line
+                Invoke-Expression -Command [string]::Join("`n", $runnable)
             }
-
-        ($runnable -join "`n") | Invoke-Expression
+            else {
+                'Not running'
+            }
         }
-        else {
-            "Not running"
-        }
+    } catch {
+        Write-Error -ErrorRecord $_ -ErrorAction $ErrorActionPreference
     }
 }
