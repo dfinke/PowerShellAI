5d145294495b26351e281826f2697702f6f0d803
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-28 13:07:46 +0100

    Fix punctuation; Splat parameters to avoid backticks; Add error handling

--- Public/Enable-AIShortCutKey.ps1
+++ Public/Enable-AIShortCutKey.ps1
@@ -1,35 +1,41 @@
 function Enable-AIShortCutKey {
     <#
         .SYNOPSIS
-        Enable the Ctrl+g shortcut key for getting completions
+        Enable the Ctrl+g shortcut key for getting completions.
 
         .EXAMPLE
         Enable-AIShortCutKey
     #>
-    Set-PSReadLineKeyHandler -Key Ctrl+g `
-        -BriefDescription OpenAICli `
-        -LongDescription "Calls Open AI on the current buffer" `
-        -ScriptBlock {
-        param($key, $arg)
+    $splatParams = @{
+        Key                 = 'Ctrl+g'
+        BriefDescription    = 'OpenAICli'
+        LongDescription     = 'Calls Open AI on the current buffer'
+        ScriptBlock         = {
+            param($key, $arg)
 
-        $line = $null
-        $cursor = $null
+            try {
+                $line = $null
+                $cursor = $null
 
-        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+                [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
 
-        $prompt = "Using PowerShell, just code: $($line)"
+                $prompt = "Using PowerShell, just code: $line"
 
-        $output = Get-GPT3Completion $prompt -max_tokens 256 
-        $output = $output -replace "`r", ""
+                $output = (Get-GPT3Completion -prompt $prompt -max_tokens 256 -ErrorAction Stop).Trim()
 
-        # check if output is not null
-        if ($null -ne $output) {        
-            foreach ($str in $output) {
-                if ($null -ne $str -and $str -ne "") {
-                    [Microsoft.PowerShell.PSConsoleReadLine]::AddLine()
-                    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($str)
+                # check if output is not null
+                if ($null -ne $output) {
+                    foreach ($str in $output) {
+                        if ($null -ne $str -and $str -ne '') {
+                            [Microsoft.PowerShell.PSConsoleReadLine]::AddLine()
+                            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($str)
+                        }
+                    }
                 }
+            } catch {
+                throw $_
             }
         }
     }
+    Set-PSReadLineKeyHandler @splatParams
 }
