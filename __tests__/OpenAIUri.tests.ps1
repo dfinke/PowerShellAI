e2b6ea95fbb03e3d6c23afe7f5c6b1681fa67002
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-28 15:23:58 +0100

    Simplify URI tests

--- __tests__/OpenAIUri.tests.ps1
+++ __tests__/OpenAIUri.tests.ps1
@@ -3,39 +3,27 @@ Import-Module "$PSScriptRoot\..\PowerShellAI.psd1" -Force
 Describe "OpenAIUri" -Tag 'OpenAIUri' {
     InModuleScope 'PowerShellAI' {
         It "Should return the OpenAI URI" {
-            $actual = $Script:OpenAIBaseURI
-
-            $actual | Should -Be 'https://api.openai.com/v1'
+            $Script:OpenAIBaseURI | Should -BeExactly 'https://api.openai.com/v1'
         }
 
         It "Should return the OpenAI Models URI" {
-            $actual = $Script:OpenAIModelsURI
-
-            $actual | Should -Be 'https://api.openai.com/v1/models'
+            $Script:OpenAIModelsURI | Should -BeExactly 'https://api.openai.com/v1/models'
         }
 
         It "Should return the OpenAI Moderations URI" {
-            $actual = $Script:OpenAIModerationsURI
-
-            $actual | Should -Be 'https://api.openai.com/v1/moderations'
+            $Script:OpenAIModerationsURI | Should -BeExactly 'https://api.openai.com/v1/moderations'
         }
 
         It "Should return the OpenAI Completions URI" {
-            $actual = $Script:OpenAICompletionsURI
-
-            $actual | Should -Be 'https://api.openai.com/v1/completions'
+            $Script:OpenAICompletionsURI | Should -BeExactly 'https://api.openai.com/v1/completions'
         }
 
         It "Should return the OpenAI Images Generations URI" {
-            $actual = $Script:OpenAIImagesGenerationsURI
-
-            $actual | Should -Be 'https://api.openai.com/v1/images/generations'
+            $Script:OpenAIImagesGenerationsURI | Should -BeExactly 'https://api.openai.com/v1/images/generations'
         }
 
         It "Should return the OpenAI Edit URI" {
-            $actual = $Script:OpenAIEditsURI
-
-            $actual | Should -Be 'https://api.openai.com/v1/edits'
+            $Script:OpenAIEditsURI | Should -BeExactly 'https://api.openai.com/v1/edits'
         }
     }
 }
