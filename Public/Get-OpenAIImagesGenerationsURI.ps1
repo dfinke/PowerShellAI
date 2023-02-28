0a834c177c057e0c4b5450b7059fe66f3d06a733
Kris Borowinski <kris.borowinski@outlook.com>
2023-02-28 13:31:39 +0100

    Fix punctuation in help and remove extra whitespaces

--- Public/Get-OpenAIImagesGenerationsURI.ps1
+++ Public/Get-OpenAIImagesGenerationsURI.ps1
@@ -1,9 +1,9 @@
 
 function Get-OpenAIImagesGenerationsURI {
     <#
-        .Synopsis
-        Base url for OpenAI Images Generations API
+        .SYNOPSIS
+        Base url for OpenAI Images Generations API.
     #>
-    
+
     (Get-OpenAIBaseRestURI) + '/images/generations'
 }
