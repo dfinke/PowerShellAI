4445cbda980532967e6c310f955eadc6cd1ed8ce
SP3269 <25411814+SP3269@users.noreply.github.com>
2023-04-02 09:29:29 +1000

    Adding Set-OpenAIModel

---
+++ Public/Set-OpenAIModel.ps1
@@ -0,0 +1,26 @@
+function Set-OpenAIModel {
+    <#
+        .SYNOPSIS
+        Set the OpenAI API model.
+
+        .DESCRIPTION
+        Sets the default OpenAI model using string parameter.
+
+        .PARAMETER Model
+        Specifies OpenAI API model name as string.
+
+        .EXAMPLE
+        Set-OpenAIModel -Model (Get-OpenAIModel | Get-Random)
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [ValidateScript({if ($_ -in (Get-OpenAIModel)) {$true} else {throw 'Sepcified OpenAI Model is not on the available models list.'}})]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Model
+    )
+
+    $Script:OpenAIModel = $Model
+    Write-Verbose "Using $Model model"
+}
