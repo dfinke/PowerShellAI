c065cc14b80fdee6520350e13417561b81ae62e8
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2023-06-17 10:34:53 -0700

    Add support for OPEN_AI_KEY env variable.

--- Private/Get-LocalOpenAIKey.ps1
+++ Private/Get-LocalOpenAIKey.ps1
@@ -11,12 +11,15 @@ function Get-LocalOpenAIKey {
             #On PowerShell 6 and higher return secure string because Invoke-RestMethod supports Bearer authentication with secure Token
             $Script:OpenAIKey
         } else {
-            #On PowerShell 5 and lower use .NET marshaling to convert the secure string to plain text
+            #On PowerShell 5 and lower use .NET marshalling to convert the secure string to plain text
             [Runtime.InteropServices.Marshal]::PtrToStringAuto(
                 [Runtime.InteropServices.Marshal]::SecureStringToBSTR($Script:OpenAIKey)
             )
         }
-    } else {
+    } elseif (Test-Path Env:\OpenAIKey) {
         $env:OpenAIKey
+    } else {
+        # https://help.openai.com/en/articles/5112595-best-practices-for-api-key-safety
+        $env:OPEN_AI_KEY
     }
 }
