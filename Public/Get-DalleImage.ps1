80f2d7476b20ff7a5e497d93a742e6b1b3722475
Dave Belt <dbelt@ap.org>
2023-11-28 14:43:23 -0500

    DALL-E 3 support

--- Public/Get-DalleImage.ps1
+++ Public/Get-DalleImage.ps1
@@ -1,59 +1,77 @@
-function Get-DalleImage { 
-    <#
-        .SYNOPSIS
-        Get a DALL-E image from the OpenAI API
-        
-        .DESCRIPTION
-        Given a description, the model will return an image
-
-        .PARAMETER Description
-        The description to generate an image for
-
-        .PARAMETER Size
-        The size of the image to generate. Defaults to 256
-
-        .PARAMETER Raw
-        If set, the raw response will be returned. Otherwise, the image will be saved to a temporary file and the path to that file will be returned
-
-        .PARAMETER NoProgress
-        The option to hide write-progress if you want, you could also set $ProgressPreference to SilentlyContinue
-
-        .EXAMPLE
-        Get-DalleImage -Description "A cat sitting on a table"
-    #>
-    [CmdletBinding()]
+function Get-DalleImage {
+    [CmdletBinding(DefaultParameterSetName = 'Dalle3')]
     param(
         [Parameter(Mandatory)]
-        $Description,
+        [string]$Description,
+
+        [Parameter(ParameterSetName = 'Dalle2')]
         [ValidateSet('256', '512', '1024')]
-        $Size = 256,
+        [string]$Size = '1024',
+
+        [Parameter(ParameterSetName = 'Dalle3')]
+        [ValidateSet('1024x1024', '1024x1792', '1792x1024')]
+        [string]$Size = '1024x1024',
+
+        [Parameter(ParameterSetName = 'Dalle3')]
+        [ValidateSet('hd', 'standard')]
+        [string]$Quality = 'hd',
+
+        [Parameter(ParameterSetName = 'Dalle3')]
+        [ValidateSet('vivid', 'natural')]
+        [string]$Style = 'natural',
+
+        [Parameter()]
+        [ValidateSet('2', '3')]
+        [string]$ModelVersion = '3',
+
         [Switch]$Raw,
         [Switch]$NoProgress
     )
 
-    $targetSize = switch ($Size) {
-        256 { '256x256' }
-        512 { '512x512' }
-        1024 { '1024x1024' }     
-    }
-  
-    $body = [ordered]@{
-        prompt = $Description
-        size   = $targetSize
-    } | ConvertTo-Json
-
-    $result = Invoke-OpenAIAPI -Uri (Get-OpenAIImagesGenerationsURI) -Body $body -Method Post
-  
-    if ($Raw) {
-        return $result
-    }
-    else {
-        $DestinationPath = [IO.Path]::GetTempFileName() -replace ".tmp", ".png"
-        $params = @{
-            Uri = $result.data.url
-            OutFile = $DestinationPath
+    begin {
+        # Adjust parameter values and defaults for DALL-E 2
+        if ($ModelVersion -eq '2') {
+            # Set model-specific default parameters
+            if (-not $PSBoundParameters.ContainsKey('Size')) {
+                $Size = '1024'
+            }
+            # Remove DALL-E 3 specific parameters
+            $PSBoundParameters.Remove('Quality') 
+            $PSBoundParameters.Remove('Style')
+            # Adjust size format for DALL-E 2
+            $Size = "$Size`x$Size"
         }
-        Invoke-RestMethodWithProgress -Params $params -NoProgress:$NoProgress
-        $DestinationPath
+
+        # Prepare the request body
+        $body = @{
+            prompt = $Description
+            size   = $Size
+            model  = "dall-e-$ModelVersion"
+        }
+
+        # Add additional parameters for DALL-E 3
+        if ($ModelVersion -eq '3') {
+            $body['quality'] = $Quality
+            $body['style'] = $Style
+        }
+    }
+
+    process {
+        $bodyJson = $body | ConvertTo-Json
+
+        $result = Invoke-OpenAIAPI -Uri (Get-OpenAIImagesGenerationsURI) -Body $bodyJson -Method Post
+
+        if ($Raw) {
+            return $result
+        }
+        else {
+            $destinationPath = [IO.Path]::GetTempFileName() -replace '\.tmp$', '.png'
+            $params = @{
+                Uri     = $result.data.url
+                OutFile = $destinationPath
+            }
+            Invoke-RestMethodWithProgress -Params $params -NoProgress:$NoProgress
+            return $destinationPath
+        }
     }
 }
