ab152e23f3d47759b3dfbf786207e941ba18af0a
Dave Belt <dbelt@ap.org>
2023-11-28 16:16:06 -0500

    Added help info to get-DalleImage

--- Public/Get-DalleImage.ps1
+++ Public/Get-DalleImage.ps1
@@ -1,4 +1,44 @@
 function Get-DalleImage {
+    <#
+    .SYNOPSIS
+    Get a DALL-E image from the OpenAI API
+    
+    .DESCRIPTION
+    Given a description, the model will return an image based on the DALL-E version specified.
+
+    .PARAMETER Description
+    The text description of the desired image.
+    
+    .PARAMETER Size
+    For DALL-E 2, the Size parameter specifies the dimensions of the square image to generate. It must be one of '256', '512', or '1024', indicating the length of the sides of the square in pixels. Defaults to '1024'.
+
+    For DALL-E 3, instead of this parameter, 'Orientation' is used to determine the aspect ratio and size of the image.
+
+    .PARAMETER Orientation
+    Used with DALL-E 3 to specify the orientation and aspect ratio of the generated image.
+    - 'landscape' will produce an image of size '1792x1024'.
+    - 'portrait' will produce an image of size '1024x1792'.
+    - 'square' will produce an image of size '1024x1024'.
+    The default is 'landscape'.
+
+    .PARAMETER Quality
+    Specific to DALL-E 3, determines the quality of the generated image. Can be 'standard' or 'hd', where 'hd' corresponds to higher detail. Defaults to 'hd'.
+
+    .PARAMETER Style
+    Specific to DALL-E 3, affects the style of the image. Can be 'vivid' for more dramatic and hyper-real images or 'natural' for more true-to-life images. Defaults to 'natural'.
+
+    .PARAMETER Raw
+    When specified, the command will return the raw API response. If omitted, the resulting image will be saved to a temporary file and its path returned.
+
+    .PARAMETER NoProgress
+    When specified, progress indicators are not displayed during the operation. Progress indicators can also be suppressed by setting $ProgressPreference to 'SilentlyContinue'.
+
+    .EXAMPLE
+    Get-DalleImage -Description "A cat sitting on a table"
+    
+    Generates an image for "A cat sitting on a table" using the default model version and settings, and returns the path to the saved image file.
+#>
+    
     [CmdletBinding(DefaultParameterSetName = 'Dalle3')]
     param(
         [Parameter(Mandatory)]
